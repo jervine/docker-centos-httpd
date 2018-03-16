a0ef37821b3e0594cb4013e421283670727ba55b
Jonny Ervine <jervine@users.noreply.github.com>
2018-03-16 16:56:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,11 +6,13 @@
 ## Initialise any variables being called:
 # Set the correct timezone
 TZ=${TZ:-UTC}
-setup=/config/httpd/.setup
+CONF_DIR=${CONF_DIR:/config/confs/}
+setup=/config/.setup
 
 if [ ! -f "${setup}" ]; then
   rm -f /etc/localtime
   cp /usr/share/zoneinfo/$TZ /etc/localtime
+  echo "IncludeOptional $CONF_DIR/*.conf" >> /etc/httpd/conf/httpd.conf
   touch $setup
 fi
 
